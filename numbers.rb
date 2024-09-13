2ca4089a88a035b9583badf4e7900351d6143ea4
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-09-13 19:49:46 +0300

    Добавлен метод для нахождения максимальной цифры числа, не делящейся на 3.

--- numbers.rb
+++ numbers.rb
@@ -8,5 +8,10 @@ end
 end
 puts "Количество чётных чисел, не взаимно простых с числом #{number}: #{count}"
 
+puts 'Введите число:'
+number = gets.chomp.to_i
+digits = number.to_s.chars.map(&:to_i)
+max_digit = digits.reject { |digit| digit % 3 == 0 }.max
+puts "Максимальная цифра числа #{number}, не делящаяся на 3: #{max_digit}"
 
 
