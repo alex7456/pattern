ab947ed8780f9393cd6e99c08aaf0a7cbb37af34
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-09-25 21:01:27 +0300

    Тестирую работу всех валидаций

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -1,6 +1,6 @@
 require_relative 'Student'  
 
-
+# Тестирование класса Student
 begin
   student1 = Student.new(id: '1', surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', phone: '111-222-333', telegram: 'ivan123', email: 'ivan@mail.ru', git: 'ivan123')
 rescue ArgumentError => e
@@ -9,7 +9,7 @@ rescue ArgumentError => e
 end
 
 begin
-  student2 = Student.new(id: '2', surname: 'Петров', name: 'Петр', patronymic: 'Петрович', phone: 'rsgfdg', telegram: 'petr123', email: 'petr@mail.ru', git: 'petr123')
+  student2 = Student.new(id: '2', surname: 'П32', name: 'Пет1', patronymic: 'Петрови23', phone: 'rsgfdg', telegram: 'вфыв', email: 'petr@mail', git: 'petr@123')
 rescue ArgumentError => e
   puts e.message
   student2 = nil
@@ -17,7 +17,7 @@ end
 
 student3 = Student.new(id: '3', surname: 'Сидоров', name: 'Егор', patronymic: 'Егорович')
 
-
+# Метод для вывода информации о студенте
 def print_student_info(student)
   if student.nil?
     puts "Студент не создан из-за ошибки в данных."
