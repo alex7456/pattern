7655e8dc0de62f272cb591936e04f67ad5b6512e
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-09-26 14:50:47 +0300

    Проверяю работу метода validate

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -3,20 +3,26 @@ require_relative 'Student'
 # Тестирование класса Student
 begin
   student1 = Student.new(id: '1', surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', phone: '111-222-333', telegram: 'ivan123', email: 'ivan@mail.ru', git: 'ivan123')
+  student1.validate
 rescue ArgumentError => e
   puts e.message
   student1 = nil
 end
 
 begin
-  student2 = Student.new(id: '2', surname: 'П32', name: 'Пет1', patronymic: 'Петрови23', phone: 'rsgfdg', telegram: 'вфыв', email: 'petr@mail', git: 'petr@123')
+  student2 = Student.new(id: '2', surname: 'Петров', name: 'Петр', patronymic: 'Петрович', git: '')  # Нет контактов, пустой гит
+  student2.validate
 rescue ArgumentError => e
   puts e.message
   student2 = nil
 end
-
-student3 = Student.new(id: '3', surname: 'Сидоров', name: 'Егор', patronymic: 'Егорович')
-
+begin
+ student3 = Student.new(id: '3', surname: 'Сидоров', name: 'Егор', patronymic: 'Егорович')
+student3.validate
+rescue ArgumentError => e
+puts e.message
+student3=nil
+end
 # Метод для вывода информации о студенте
 def print_student_info(student)
   if student.nil?
