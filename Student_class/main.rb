8d66f0ba5ff06d0ef3387a7fdb4f70e75318dd78
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-09-27 11:59:59 +0300

    проверяю работу метода set_contacts

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -1,8 +1,7 @@
-require_relative 'Student'  
-
-# Тестирование класса Student
+require_relative "Student"
 begin
-  student1 = Student.new(id: '1', surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', phone: '111-222-333', telegram: 'ivan123', email: 'ivan@mail.ru', git: 'ivan123')
+  student1 = Student.new(id: '1', surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', git: 'ivan123')
+  student1.set_contacts(phone: '111-222-333', telegram: 'ivan123', email: 'ivan@mail.ru')
   student1.validate
 rescue ArgumentError => e
   puts e.message
@@ -10,26 +9,30 @@ rescue ArgumentError => e
 end
 
 begin
-  student2 = Student.new(id: '2', surname: 'Петров', name: 'Петр', patronymic: 'Петрович', git: '')  # Нет контактов, пустой гит
+  student2 = Student.new(id: '2', surname: 'Петров', name: 'Петр', patronymic: 'Петрович', git: 'petr123')
+  student2.set_contacts(phone: '222-333-444')
   student2.validate
 rescue ArgumentError => e
   puts e.message
   student2 = nil
 end
+
 begin
- student3 = Student.new(id: '3', surname: 'Сидоров', name: 'Егор', patronymic: 'Егорович')
-student3.validate
+  student3 = Student.new(id: '3', surname: 'Сидоров', name: 'Егор', patronymic: 'Егорович', git:'egor123')
+  student3.set_contacts(email: 'egor@example.com')
+  student3.validate
 rescue ArgumentError => e
-puts e.message
-student3=nil
+  puts e.message
+  student3 = nil
 end
-# Метод для вывода информации о студенте
+
+
 def print_student_info(student)
   if student.nil?
     puts "Студент не создан из-за ошибки в данных."
     return
   end
-  
+
   puts "ID: #{student.id}"
   puts "ФИО: #{student.surname} #{student.name} #{student.patronymic}"
   puts "Телефон: #{student.phone}" if student.phone
