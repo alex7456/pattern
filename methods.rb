a269a63924d832e33592f6e3f42b77de27bea723
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-09-28 08:31:32 +0300

    Update methods.rb

--- methods.rb
+++ methods.rb
@@ -1,23 +1,29 @@
+def find_min(arr)
+  min = arr[0]
+  for i in 1...arr.length
+    if arr[i] < min
+      min = arr[i]
+    end
+  end
+  min
+end
+
+def find_first_positive_index(arr)
+  index = -1
+  i = 0
+  while i < arr.length
+    if arr[i] > 0
+      index = i
+      break
+    end
+    i += 1
+  end
+  index
+end
 array = [-3, -1, 0, 4, -5, 2]
 
-
-min_element = array[0]
-for i in 1...array.length
-  if array[i] < min_element
-    min_element = array[i]
-  end
-end
-
-
-first_positive_index = -1
-i = 0
-while i < array.length
-  if array[i] > 0
-    first_positive_index = i
-    break
-  end
-  i += 1
-end
+min_element = find_min(array)
+first_positive_index = find_first_positive_index(array)
 
 puts "Минимальный элемент: #{min_element}"
 puts "Номер первого положительного элемента: #{first_positive_index}"
