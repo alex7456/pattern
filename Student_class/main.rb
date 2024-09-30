7cab9757a02a02ebac82be92e70b9c8186241c68
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-09-30 20:40:57 +0300

    Testing Student_short t.2.4

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -1,4 +1,5 @@
 require_relative 'Student'
+require_relative 'Student_short'
 
 begin
   student1 = Student.new(id: '1', surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', git: 'ivan123')
@@ -19,13 +20,21 @@ rescue ArgumentError => e
 end
 
 begin
-  student3 = Student.new(id: '3', surname: 'Сидоров', name: 'Егор', patronymic: 'Егорович', git:'egor123')
+  student3 = Student.new(id: '3', surname: 'Сидоров', name: 'Егор', patronymic: 'Егорович', git: 'egor123')
   student3.set_contacts(email: 'egor@example.com')
   student3.validate
 rescue ArgumentError => e
   puts e.message
   student3 = nil
 end
+
+
+student4 = Student.new(id: '4', surname: 'Селезнев', name: 'Александр', patronymic: 'Александрович', git: 'as123')
+student4.set_contacts(phone: '123-456-789')  
+
+student_short1 = Student_short.new(student4)
+
+student_short2 = Student_short.new('5', 'Александров А.А., petrGit, Phone: 987-654-321')
 
 def print_student_info(student)
   if student.nil?
@@ -40,3 +49,6 @@ end
 print_student_info(student1)
 print_student_info(student2)
 print_student_info(student3)
+
+puts "Student_short1: ID=#{student_short1.id}, Surname Initials=#{student_short1.surname_initials}, Git=#{student_short1.git}, Contact=#{student_short1.contact}"
+puts "Student_short2: ID=#{student_short2.id}, Surname Initials=#{student_short2.surname_initials}, Git=#{student_short2.git}, Contact=#{student_short2.contact}"
