05fd8dddedc5ad5d051217ac4d4907176e7038e2
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-10-01 13:45:58 +0300

    Update Student_short.rb t.2.5
    Наследуем класс от базового, избегаем дублироавния

--- Student_class/Student_short.rb
+++ Student_class/Student_short.rb
@@ -1,42 +1,33 @@
-class Student_short
-  attr_reader :id, :surname_initials, :git, :contact
-
+class Student_short < PersonBase
   def initialize(arg1, arg2 = nil)
-    if arg2.nil? && arg1.is_a?(Student)
-      
+    if arg1.is_a?(Student)
       student = arg1
-      @id = student.id
-      @surname_initials = "#{student.surname} #{student.name[0]}.#{student.patronymic[0]}."
-      @git = student.git
-      @contact = extract_contact(student) 
-    elsif arg2.is_a?(String)
-      @id = arg1
-      parse_info_str(arg2)
+      super(id: student.id, surname_initials: student.surname_initials, git: student.git)
+      @contact = student.get_contact
+    elsif arg1.is_a?(String) && arg2.is_a?(String)
+      super(id: arg1, surname_initials: parse_surname_initials(arg2), git: parse_git(arg2))
+      @contact = parse_contact(arg2)
     else
-      raise ArgumentError, "Неверные аргументы для инициализации Student_short"
+      raise ArgumentError, "Неверные аргументы для создания Student_short"
     end
+  end
+
+  def get_contact
+    @contact || "Нет контактов"
   end
 
   private
 
-  
-  def extract_contact(student)
-    contacts = []
-    contacts << "Телефон: #{student.phone}" if student.phone
-    contacts << "Email: #{student.email}" if student.email
-    contacts << "Телеграм: #{student.telegram}" if student.telegram
-    contacts.empty? ? "Нет контактов" : contacts.join(", ")  
+  def parse_surname_initials(info_str)
+    info_str.split(',').map(&:strip)[0]
   end
 
-  def parse_info_str(info_str)
-    info_parts = info_str.split(',').map(&:strip)
+  def parse_git(info_str)
+    info_str.split(',').map(&:strip)[1]
+  end
 
-    if info_parts.size != 3
-      raise ArgumentError, "Неправильный формат строки"
-    end
-
-    @surname_initials = info_parts[0] 
-    @git = info_parts[1]              
-    @contact = info_parts[2]          
+  def parse_contact(info_str)
+    info_str.split(',').map(&:strip)[2]  
   end
 end
+
