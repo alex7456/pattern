9df4dd39c7cec6cffbc19bd042eb90a6db8f53ae
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-10-01 13:49:44 +0300

    testing t.2.5

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -28,13 +28,12 @@ rescue ArgumentError => e
   student3 = nil
 end
 
-
 student4 = Student.new(id: '4', surname: 'Селезнев', name: 'Александр', patronymic: 'Александрович', git: 'as123')
-student4.set_contacts(phone: '123-456-789')  
+student4.set_contacts(phone: '123-456-789')
 
 student_short1 = Student_short.new(student4)
 
-student_short2 = Student_short.new('5', 'Александров А.А., petrGit, Phone: 987-654-321')
+student_short2 = Student_short.new('5', 'Александров А.А., petrGit, Телефон: 987-654-321')
 
 def print_student_info(student)
   if student.nil?
@@ -50,5 +49,5 @@ print_student_info(student1)
 print_student_info(student2)
 print_student_info(student3)
 
-puts "Student_short1: ID=#{student_short1.id}, Surname Initials=#{student_short1.surname_initials}, Git=#{student_short1.git}, Contact=#{student_short1.contact}"
-puts "Student_short2: ID=#{student_short2.id}, Surname Initials=#{student_short2.surname_initials}, Git=#{student_short2.git}, Contact=#{student_short2.contact}"
+puts "Student_short1: ID=#{student_short1.id}, Surname Initials=#{student_short1.surname_initials}, Git=#{student_short1.git}, Contact=#{student_short1.get_contact}"
+puts "Student_short2: ID=#{student_short2.id}, Surname Initials=#{student_short2.surname_initials}, Git=#{student_short2.git}, Contact=#{student_short2.get_contact}"
