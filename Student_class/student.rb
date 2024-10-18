7f15718aef92368249425bbc0d2e9ab25ef0b6b2
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-10-18 19:26:53 +0300

    Update student.rb

--- Student_class/student.rb
+++ Student_class/student.rb
@@ -1,21 +1,33 @@
+# student.rb
 require_relative 'PersonBase'
+
 class Student < PersonBase
-  
-  attr_reader :id, :surname_initials, :git
+  attr_reader :surname_initials
 
-  def initialize(id:, surname:, name:, patronymic:, git:)
-    @id = id
-    @git = git
+  def initialize(id:, git:, surname:, name:, patronymic:)
+    self.id = id  # Используем сеттер с валидацией
+    self.git = git  # Используем сеттер с валидацией
 
-    validate_git(git)
+    # Валидация ФИО
     validate_fio(surname, name, patronymic)
 
-    # Сохраняем только необходимые данные
+    # Сохраняем фамилию с инициалами
     @surname_initials = generate_initials(surname, name, patronymic)
+
+   
+  
   end
 
   def get_full_info
     "#{surname_initials}; Git: #{git}; Связь: #{get_contact}"
+  end
+
+  def get_contact
+    contact_info = []
+    contact_info << "Телефон: #{phone}" if phone
+    contact_info << "Email: #{email}" if email
+    contact_info << "Телеграм: #{telegram}" if telegram
+    contact_info.empty? ? "Нет контактов" : contact_info.join(", ")
   end
 
   private
@@ -24,16 +36,14 @@ class Student < PersonBase
     "#{surname} #{name[0]}.#{patronymic[0]}."
   end
 
-  def validate_git(git)
-    raise ArgumentError, "Необходимо указать Git" if git.nil? || git.empty?
-  end
-
+  # Валидация ФИО
   def validate_fio(surname, name, patronymic)
     { 'Фамилия' => surname, 'Имя' => name, 'Отчество' => patronymic }.each do |field_name, field_value|
       validate_name_part(field_value, field_name)
     end
   end
 
+  # Валидация отдельных частей ФИО
   def validate_name_part(name_part, field_name)
     if name_part.nil? || name_part.strip.empty?
       raise ArgumentError, "#{field_name} не может быть пустым"
