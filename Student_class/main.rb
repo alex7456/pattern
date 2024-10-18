44c90c595b81467172b6e8ccedbdcda11b068c4d
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-10-18 17:31:50 +0300

    Update main.rb

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -1,28 +1,22 @@
+require_relative 'PersonBase'
 require_relative 'Student'
 require_relative 'Student_short'
 
 begin
   student1 = Student.new(id: '1', surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', git: 'ivan123')
-  student1.set_contacts(phone: '111-222-333', telegram: 'ivan123', email: 'ivan@mail.ru')
-  student1.validate
+  student1.set_contact(phone: '111-222-333', telegram: 'ivan123', email: 'ivan@mail.ru')
 rescue ArgumentError => e
   puts e.message
-  student1 = nil
 end
+
 begin
   student2 = Student.new(id: '2', surname: 'Стройный', name: 'Александр', patronymic: 'Александрович', git: 'alex7456')
-  student2.set_contacts( telegram: 'alex')
-  student2.validate
+  student2.set_contact(telegram: 'alex')
 rescue ArgumentError => e
   puts e.message
-  student2 = nil
 end
 
-
-
-
 student_short1 = Student_short.new(student2)
-
 
 def print_student_info(student)
   if student.nil?
@@ -35,6 +29,5 @@ def print_student_info(student)
 end
 
 print_student_info(student1)
-
 
 puts "Student_short1: ID=#{student_short1.id}, Surname Initials=#{student_short1.surname_initials}, Git=#{student_short1.git}, Contact=#{student_short1.get_contact}"
