8552c27f2bfa0eea86436aea023145978eb291f7
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-10-23 16:02:25 +0300

    add second method 3.1.2

--- polymorhism_blocks_collections_tests/solver.rb
+++ polymorhism_blocks_collections_tests/solver.rb
@@ -4,7 +4,20 @@ def descending_indices(data, input_type)
   result = array.each_with_index.sort_by { |val, index| -val }.map { |val, index| index }
   puts "Индексы в порядке убывания значений: #{result}"
 end
+# 2. Элементы между первым и вторым максимальными
+def elements_between_first_and_second_max(data, input_type)
+  array = get_array_input(data, input_type)
+  max1, max2 = array.max(2)
 
+  idx1 = array.index(max1)  # Индекс первого максимального
+  idx2 = array.rindex(max2) # Индекс второго максимального
+  
+  if idx1 && idx2 && idx2 > idx1
+    puts "Элементы между первым и вторым максимальными: #{array[(idx1 + 1)...idx2]}"
+  else
+    puts "Невозможно найти элементы между первым и вторым максимальными, так как они расположены в некорректном порядке."
+  end
+end
 
 # Вспомогательные методы для получения ввода
 def get_array_input(data, input_type)
