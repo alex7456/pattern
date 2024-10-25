797b1d78fe4f110e1cc36aa32e1eadbf6806093c
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-10-25 17:11:10 +0300

    add constructors

--- Student_class/student_short.rb
+++ Student_class/student_short.rb
@@ -1,26 +1,27 @@
 require_relative 'Student'
-
-class Student_short < PersonBase
+class StudentShort < PersonBase
   attr_reader :fio, :contact
 
-  def initialize(student: nil, id: nil, student_str: nil)
-    if student
-      super(id: student.id, git: student.git)
-      short_setter(student.get_info)
-    elsif id && student_str
-      super(id: id)
-      short_setter(student_str)
-    else
-      raise ArgumentError, "Invalid arguments for StudentShort"
-    end
+  def self.from_student(student)
+    new(id: student.id, git: student.git, fio: "#{student.surname} #{student.initials}", contact: student.get_contact)
   end
 
-  private
+  def self.from_string(id:, student_str:)
+    info_arr = student_str.split("\t")
+    new(id: id, git: info_arr[1], fio: info_arr[0], contact: info_arr[2])
+  end
 
-  def short_setter(info)
-    info_arr = info.split("\t")
-    set_attribute(:fio, info_arr[0])
-    set_attribute(:git, info_arr[1])
-    set_attribute(:contact, info_arr[2])
+  def initialize(id:, git: nil, fio:, contact: nil)
+    super(id: id, git: git)
+    self.fio = fio
+    self.contact = contact
+  end
+
+  def fio=(value)
+    set_attribute(:fio, value)
+  end
+
+  def contact=(value)
+    set_attribute(:contact, value)
   end
 end
