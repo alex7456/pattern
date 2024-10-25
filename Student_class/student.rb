d55cbfa299cdc823559b0adc9a3da919d1857dfa
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-10-25 17:09:26 +0300

    add setters

--- Student_class/student.rb
+++ Student_class/student.rb
@@ -2,20 +2,42 @@ require_relative 'PersonBase'
 class Student < PersonBase
   attr_reader :email, :phone, :telegram, :first_name, :surname, :last_name
 
-  def initialize(first_name:, surname:, last_name:, phone: nil, telegram: nil, git:, email: nil, id: nil)
+  def initialize(first_name:, surname:, last_name:, phone: nil, telegram: nil, git: nil, email: nil, id: nil)
     super(id: id, git: git)
-    raise "FIO required" if first_name.nil? || surname.nil? || last_name.nil?
+    self.first_name = first_name
+    self.surname = surname
+    self.last_name = last_name
+    set_contacts(phone: phone, telegram: telegram, email: email)
+  end
 
-    set_attribute(:first_name, first_name) 
-    set_attribute(:surname, surname)
-    set_attribute(:last_name, last_name)
-    set_contacts(phone: phone, telegram: telegram, email: email)   
+  def first_name=(value)
+    set_attribute(:first_name, value)
+  end
+
+  def surname=(value)
+    set_attribute(:surname, value)
+  end
+
+  def last_name=(value)
+    set_attribute(:last_name, value)
+  end
+
+  def email=(value)
+    set_attribute(:email, value)
+  end
+
+  def phone=(value)
+    set_attribute(:phone, value)
+  end
+
+  def telegram=(value)
+    set_attribute(:telegram, value)
   end
 
   def set_contacts(phone: nil, telegram: nil, email: nil)
-    set_attribute(:phone, phone) if phone
-    set_attribute(:telegram, telegram) if telegram
-    set_attribute(:email, email) if email
+    self.phone = phone if phone
+    self.telegram = telegram if telegram
+    self.email = email if email
   end
 
   def get_info
