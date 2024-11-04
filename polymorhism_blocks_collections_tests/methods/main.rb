e9d359f908aaf4b11890ffcb5452ca6a5afcebe6
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-11-04 12:30:44 +0300

    add main.rb

---
+++ polymorhism_blocks_collections_tests/methods/main.rb
@@ -0,0 +1,13 @@
+require_relative 'ArrayProcessor'
+def main
+  processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+
+  puts "Элементы массива: #{processor.elements}"
+  puts "Чанк по четности: #{processor.chunk { |n| n.even? }}"
+  puts "Включает 3? #{processor.include?(3)}"
+  puts "Сумма всех элементов: #{processor.sum}"
+  puts "Член массива 10? #{processor.member?(10)}"
+  puts "Фильтр элементов больше 3: #{processor.filter { |n| n > 3 }}"
+end
+
+main
