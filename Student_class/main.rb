7b009446aa67f49125c8adbdf25a41dba0b863b1
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-11-22 19:16:46 +0300

    lab 2 refact

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -1,37 +1,26 @@
-require_relative 'PersonBase'
+require_relative 'Human'
 require_relative 'Student'
 require_relative 'Student_short'
 
-begin
-  student1 = Student.new(id: '1', surname: 'Иванов', first_name: 'Иван', last_name: 'Иванович', git: 'ivan123')
-  student1.set_contacts(phone: '111-222-333', telegram: 'ivan1234', email: 'ivan@mail.ru')
-rescue ArgumentError => e
-  puts e.message
-end
+student_info = {
+  id: 1,
+  first_name: "Иван",
+  surname: "Иванов",
+  last_name: "Иванович",
+  phone: "111-222-333",
+  telegram: "@ivanov",
+  email: "ivanov@ya.ru",
+  github: "https://github.com/ivanov"
+}
 
-begin
-  student2 = Student.new(id: '2', surname: 'Стройный', first_name: 'Александр', last_name: 'Александрович', git: 'alex7456')
-  student2.set_contacts(telegram: 'alex7456')
-rescue ArgumentError => e
-  puts e.message
-end
+student = Student.new(**student_info)
+puts "#{student}, #{student.validate?}"
 
-begin
-  student_short1 = Student_short.new(student: student2)
-  puts "Student_short1: ID=#{student_short1.id}, FIO=#{student_short1.fio}, Git=#{student_short1.git}, Contact=#{student_short1.contact}"
-rescue ArgumentError => e
-  puts e.message
-end
+# Данные из строки и проверка на валидацию
+string = "ID: 2; Инициалы: ТестовТ.Т.; GitHub: https://github.com/test; Контакт: 111-222-333"
+student_short = StudentShort.from_string(string)
+puts "#{student_short}, #{student_short.validate?}"
 
-# Функция для вывода информации о студенте
-def print_student_info(student)
-  if student.nil?
-    puts "Студент не создан из-за ошибки в данных."
-    return
-  end
-
-  puts student.get_info
-  puts "-" * 20
-end
-
-print_student_info(student1)
+# Данные из объекта Student и проверка на валидацию
+student_short = StudentShort.from_student(student)
+puts "#{student_short}, #{student_short.validate?}"
