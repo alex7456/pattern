6fa2def2f5b35c0d5c49df72d197ec66743ae61c
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-05 22:32:46 +0300

    update L3T2

--- polymorhism_blocks_collections_tests/methods/main.rb
+++ polymorhism_blocks_collections_tests/methods/main.rb
@@ -8,6 +8,6 @@ def main
   puts "Сумма всех элементов: #{processor.sum}"
   puts "Член массива 10? #{processor.member?(10)}"
   puts "Фильтр элементов больше 3: #{processor.filter { |n| n > 3 }}"
+  puts "Сумма элементов с reduce: #{processor.reduce(0) { |sum, n| sum + n }}"
 end
-
 main
