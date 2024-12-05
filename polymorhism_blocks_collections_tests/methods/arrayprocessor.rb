1cf6ce446a05385d6827056045825e06f80023e0
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-05 22:32:16 +0300

    update L3T2

--- polymorhism_blocks_collections_tests/methods/arrayprocessor.rb
+++ polymorhism_blocks_collections_tests/methods/arrayprocessor.rb
@@ -1,14 +1,15 @@
 class ArrayProcessor
-def initialize(array)
-	@array = array.dup.freeze
-end
-def elements
-@array.dup
-end
-def chunk
-result =[]
-chunk=[]
- @array.each do |item|
+  attr_reader :array
+  def initialize(array)
+    @array = array
+  end
+  def elements
+    array.dup
+  end
+  def chunk
+    chunk = []
+    result = []
+    array.each do |item|
       if chunk.empty? || yield(chunk.last) == yield(item)
         chunk << item
       else
@@ -17,42 +18,45 @@ chunk=[]
       end
     end
     result << chunk unless chunk.empty?
-
     result
   end
-def include?(value)
-@array.each do |item|
-return true if item==value 
-end
-false
-end
-def reduce(initial = nil)
+  def include?(value)
+    array.each do |item|
+      return true if item == value
+    end
+  end
+  false
+  def reduce(initial = nil)
     accumulator = initial
-    @array.each do |item|
-      accumulator = accumulator.nil? ? item : yield(accumulator, item)
+    array.each do |item|
+      if accumulator.nil?
+        accumulator = item
+      else
+        accumulator = yield(accumulator, item)
+      end
+
+
     end
     accumulator
   end
-   def sum
+  def sum
     total = 0
-    @array.each do |item|
+    array.each do |item|
       total += item
     end
     total
   end
   def member?(value)
-    @array.each do |item|
+    array.each do |item|
       return true if item == value
     end
     false
   end
-  
-   def filter
+  def filter
     result = []
-    @array.each do |item|
+    array.each do |item|
       result << item if yield(item)
     end
     result
   end
 end
-  
