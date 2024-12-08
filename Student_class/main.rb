a9ff513060c3b23bb802deb4f638763e814d6d88
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-08 16:50:50 +0300

    Update main.rb L3T4

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -1,7 +1,7 @@
 require_relative 'Human'
 require_relative 'Student'
 require_relative 'Student_short'
-
+require_relative 'student_tree'
 student_info = {
   id: 1,
   first_name: "Иван",
@@ -10,17 +10,27 @@ student_info = {
   phone: "111-222-333",
   telegram: "@ivanov",
   email: "ivanov@ya.ru",
-  github: "https://github.com/ivanov"
+  git: "https://github.com/ivanov",
+  birthdate:"2000-05-15"
 }
-
 student = Student.new(**student_info)
-puts "#{student}, #{student.validate?}"
+puts "#{student}#{student.validate?}"
 
-# Данные из строки и проверка на валидацию
 string = "ID: 2; Инициалы: ТестовТ.Т.; GitHub: https://github.com/test; Контакт: 111-222-333"
-student_short = StudentShort.from_string(string)
-puts "#{student_short}, #{student_short.validate?}"
+short_student = Student_short.from_string(string)
+puts "#{short_student}#{short_student.validate?}"
 
-# Данные из объекта Student и проверка на валидацию
-student_short = StudentShort.from_student(student)
-puts "#{student_short}, #{student_short.validate?}"
+sheet_student=Student_short.from_student(student)
+puts "#{sheet_student}#{sheet_student.validate?}"
+
+student1 = Student.new(first_name: "Иван", surname: "Иванов", last_name: "Иванович", birthdate: "2000-05-15")
+student2 = Student.new(first_name: "Петр", surname: "Петров", last_name: "Петрович", birthdate: "1998-08-20")
+student3 = Student.new(first_name: "Алексей", surname: "Алексеев", last_name: "Алексеевич", birthdate: "2002-03-10")
+student4 = Student.new(first_name: "Мария", surname: "Сидорова", last_name: "Александровна", birthdate: "1997-12-31")
+tree = StudentTree.new
+tree.add(student1)
+tree.add(student2)
+tree.add(student3)
+tree.add(student4)
+puts "Все студенты в порядке даты рождения:"
+tree.each { |student| puts "#{student.surname} #{student.birthdate}" }
