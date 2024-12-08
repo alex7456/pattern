cadf51ecbc7d8b9852978d2f3e6eb3f4713717db
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-08 12:43:12 +0300

    lab 2 refact

--- Student_class/student_short.rb
+++ Student_class/student_short.rb
@@ -1,12 +1,10 @@
-class StudentShort < Human
-  attr_reader :fio, :contact
-
-  def initialize(fio:, contact:, id: nil, github: nil)
-    super(id: id, github: github)
-    @fio = fio
+class Student_short < Human
+  attr_reader :initials, :contact
+  def initialize(initials:, contact:, id:nil, git:nil)
+    super(id:id,git:git)
+    @initials= initials
     @contact = contact
   end
-
   def self.from_string(string)
     data = {}
     string.scan(/([^:;]+): ([^;]+)(?:;|$)/).each do |key, value|
@@ -17,42 +15,39 @@ class StudentShort < Human
       when "id"
         data[:id] = value.to_i
       when "инициалы"
-        data[:fio] = value
+        data[:initials] = value
       when "github"
-        data[:github] = value
+        data[:git] = value
       when "контакт"
         data[:contact] = value
       end
     end
 
 
-    raise ArgumentError, "Missing required data: :fio or :contact" unless data[:fio] && data[:contact]
+    raise ArgumentError, "Missing required data: :fio or :contact" unless data[:initials] && data[:contact]
 
     new(**data)
   end
-
-
   def self.from_student(student)
     new(
-      fio: student.initials,
-      contact: student.contact,
-      id: student.id,
-      github: student.github
-    )
-  end
+      initials: student.initials,
+    contact: student.contact,
+    id: student.id,
+    git: student.git,
 
+    )
+
+  end
   def to_s
     data = []
-    data << "id: #{@id}" if id
-    data << "Инициалы: #{@fio}" if fio
-    data << @contact if contact
-    data << "Git: #{@github}" if github
-    data.join("; ")
+    data << "ID: #{@id}" if id
+    data << "FIO: #{@initials}" if initials
+    data << "Contact: #{@contact}" if contact
+    data << "Git: #{@git}" if git
+    data.join("\n")
   end
-
   def validate?
-    !github.nil? && !github.empty? && !contact.nil? && !contact.empty?
+    !git.nil? && !git.empty? && !contact.nil? && !contact.empty?
   end
-
   private_class_method :new
 end
