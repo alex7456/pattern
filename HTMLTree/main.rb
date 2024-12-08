77cfd1f4a629c1915c9d79c5fa8c074b5ddbbd99
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-08 13:55:50 +0300

    refact L3T3

--- HTMLTree/main.rb
+++ HTMLTree/main.rb
@@ -1,10 +1,23 @@
 require_relative 'HtmlTree'
 
-html_content = "<div><p>Text</p><span>Other</span></div>"
-tree = HtmlTree.new(html_content)
+html = "
+<html>
+	<body>
+		<div class=\"container\">
+			<p>Hello, world!</p>
+		</div>
+		<div>
+		</div>
+	</body>
+</html>"
 
-puts "Обход в глубину:"
-tree.each { |tag| puts tag.name }
-
-puts "Обход в ширину:"
-tree.each_breadth_first { |tag| puts tag.name }
+tree = HtmlTree.new(html)
+puts "\nМетод count для дерева: #{tree.count}"
+puts "\nМетод find для дерева (условие - имя 'body'): #{tree.find{|x| x.name = "body"}.to_s}"
+puts "\nМетод count с условием (имя div): #{tree.count{|x| x.name == "div"}}"
+puts "\nКоличество детей у первого наследника корня: #{tree.root.children[0].count_children}"
+puts "\nВывод экземпляра дерева в формате строки: #{tree.root}"
+puts "\nВывод всех элементов дерева с помощью обхода в глубину: "
+tree.each{|node| puts node}
+puts "\nМетод has_children для всех элементов дерева: "
+tree.each{|node| puts "#{node.name}: #{node.has_children?}"}
