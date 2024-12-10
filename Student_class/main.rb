de522783821668636f3c81fdb0a6582aa06cc088
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-10 19:44:37 +0300

    Update main.rb L4T4.1.8

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -2,6 +2,9 @@ require_relative 'Human'
 require_relative 'Student'
 require_relative 'Student_short'
 require_relative 'student_tree'
+require_relative 'data_table'
+require_relative 'data_list'
+require_relative 'data_list_student_short'
 student_info = {
   id: 1,
   first_name: "Иван",
@@ -23,8 +26,8 @@ puts "#{short_student}#{short_student.validate?}"
 sheet_student=Student_short.from_student(student)
 puts "#{sheet_student}#{sheet_student.validate?}"
 
-student1 = Student.new(first_name: "Иван", surname: "Иванов", last_name: "Иванович", birthdate: "2000-05-15")
-student2 = Student.new(first_name: "Петр", surname: "Петров", last_name: "Петрович", birthdate: "1998-08-20")
+student1 = Student.new(first_name: "Иван", surname: "Иванов", last_name: "Иванович",telegram: "@ivaaaaaa", birthdate: "2000-05-15")
+student2 = Student.new(first_name: "Петр", surname: "Петров", last_name: "Петрович",telegram: "@peeeeeee", birthdate: "1998-08-20")
 student3 = Student.new(first_name: "Алексей", surname: "Алексеев", last_name: "Алексеевич", birthdate: "2002-03-10")
 student4 = Student.new(first_name: "Мария", surname: "Сидорова", last_name: "Александровна", birthdate: "1997-12-31")
 tree = StudentTree.new
@@ -34,3 +37,23 @@ tree.add(student3)
 tree.add(student4)
 puts "Все студенты в порядке даты рождения:"
 tree.each { |student| puts "#{student.surname} #{student.birthdate}" }
+
+# Создание студентов
+studentshort1=Student_short.from_student(student)
+studentshort3=Student_short.from_string(string)
+list_student_short = Data_list_student_short.new([studentshort1, studentshort3])
+# Тестируем выбор студентов
+list_student_short.select(0)
+puts "Выбранные: #{list_student_short.get_selected}"
+
+list_student_short.select(1)
+puts "Выбранные: #{list_student_short.get_selected}"
+
+
+puts list_student_short.get_names
+# Получаем данные
+puts "Таблица выбранных студентов:"
+table = list_student_short.get_data
+puts table.to_s
+
+
