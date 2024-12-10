d23ad3d6c32c5f43887f9176db07a2242a7cb196
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-10 19:16:35 +0300

    update data_list_student_short.rb pattern pattern L4T4.1.7

--- Student_class/data_list_student_short.rb
+++ Student_class/data_list_student_short.rb
@@ -1,20 +1,17 @@
 require_relative 'data_list'
 require_relative 'data_table'
 class Data_list_student_short < DataList
-  # Возвращает массив имён атрибутов объектов (кроме ID)
-  def get_names
+
+  def extract_names
     %w[initials contact git]
   end
 
-  def get_data
-    rows = data.map.with_index(1) do |student, index|
-      [
-        index,
-        student.initials,
-        student.contact,
-        student.git
-      ]
-    end
-    Data_table.new(rows)
+  def extract_data(student, index)
+    [
+      index,
+      student.initials,
+      student.contact,
+      student.git
+    ]
   end
-end
+  end
