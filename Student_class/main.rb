a9ddfa3c4ecd6f1c35d83815dfb3f018111fd1e5
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-11 22:50:27 +0300

    L4T2.2 test new methods

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -1,33 +1,14 @@
-require_relative 'Human'
+require 'json'
 require_relative 'Student'
 require_relative 'Student_short'
 require_relative 'student_tree'
-require_relative 'data_table'
-require_relative 'data_list'
-require_relative 'data_list_student_short'
-student_info = {
-  id: 1,
-  first_name: "Иван",
-  surname: "Иванов",
-  last_name: "Иванович",
-  phone: "111-222-333",
-  telegram: "@ivanov",
-  email: "ivanov@ya.ru",
-  git: "https://github.com/ivanov",
-  birthdate:"2000-05-15"
-}
-student = Student.new(**student_info)
-puts "#{student}#{student.validate?}"
-
-string = "ID: 2; Инициалы: ТестовТ.Т.; GitHub: https://github.com/test; Контакт: 111-222-333"
-short_student = Student_short.from_string(string)
-puts "#{short_student}#{short_student.validate?}"
-
-sheet_student=Student_short.from_student(student)
-puts "#{sheet_student}#{sheet_student.validate?}"
-
-student1 = Student.new(first_name: "Иван", surname: "Иванов", last_name: "Иванович",telegram: "@ivaaaaaa", birthdate: "2000-05-15")
-student2 = Student.new(first_name: "Петр", surname: "Петров", last_name: "Петрович",telegram: "@peeeeeee", birthdate: "1998-08-20")
+require_relative 'DataList/data_table'
+require_relative 'DataList/data_list'
+require_relative 'DataList/data_list_student_short'
+require_relative 'Human'
+require_relative 'student_list_json'
+student1 = Student.new(first_name: "Иван", surname: "Иванов", last_name: "Иванович", birthdate: "2000-05-15")
+student2 = Student.new(first_name: "Петр", surname: "Петров", last_name: "Петрович",birthdate: "1998-08-20")
 student3 = Student.new(first_name: "Алексей", surname: "Алексеев", last_name: "Алексеевич", birthdate: "2002-03-10")
 student4 = Student.new(first_name: "Мария", surname: "Сидорова", last_name: "Александровна", birthdate: "1997-12-31")
 tree = StudentTree.new
@@ -37,6 +18,8 @@ tree.add(student3)
 tree.add(student4)
 puts "Все студенты в порядке даты рождения:"
 tree.each { |student| puts "#{student.surname} #{student.birthdate}" }
+filtr_students=tree.select{|student| student.birthdate > "1997-12-31"}
+filtr_students.each {|student| puts student}
 
 # Создание студентов
 studentshort1=Student_short.from_student(student)
@@ -55,5 +38,99 @@ puts list_student_short.get_names
 puts "Таблица выбранных студентов:"
 table = list_student_short.get_data
 puts table.to_s
+
+# Создаем файл JSON для тестирования
+TEST_JSON_FILE = 'students_test.json'
+
+# Создаем тестовый JSON-файл, если он не существует
+unless File.exist?(TEST_JSON_FILE)
+  test_data = [
+    {
+      id: 1,
+      first_name: "Иван",
+      last_name: "Иванов",
+      surname: "Иванович",
+      phone: "111-222-333",
+      telegram: "@ivanov",
+      email: "ivanov@ya.ru",
+      git: "https://github.com/ivanov",
+      birthdate: "2000-05-15"
+    },
+    {
+      id: 2,
+      first_name: "Петр",
+      last_name: "Петров",
+      surname: "Петрович",
+      phone: "222-333-444",
+      telegram: "@petrov",
+      email: "petrov@ya.ru",
+      git: "https://github.com/petrov",
+      birthdate: "1998-08-20"
+    }
+  ]
+  File.write(TEST_JSON_FILE, JSON.pretty_generate(test_data))
+end
+
+# Создаем объект для работы с JSON
+students_list = StudentsListJSON.new(TEST_JSON_FILE)
+
+# Загружаем и выводим список студентов
+puts "Загруженные студенты:"
+students_list.count.times do |i|
+  student = students_list.find_by_id(i + 1)
+  puts student
+end
+
+# Добавляем нового студента
+puts "\nДобавляем нового студента:"
+new_student = Student.new(
+  first_name: "Мария",
+  last_name: "Сидорова",
+  surname: "Александровна",
+  phone: "333-444-555",
+  telegram: "@sidorova",
+  email: "sidorova@ya.ru",
+  git: "https://github.com/sidorova",
+  birthdate: "1997-12-31"
+)
+students_list.add_student(new_student)
+
+# Вывод списка студентов после добавления
+puts "\nСписок студентов после добавления нового:"
+students_list.count.times do |i|
+  student = students_list.find_by_id(i + 1)
+  puts student
+end
+
+# Удаляем студента по ID
+puts "\nУдаляем студента с ID 2:"
+students_list.delete_by_id(2)
+
+# Вывод списка студентов после удаления
+puts "\nСписок студентов после удаления:"
+students_list.count.times do |i|
+  student = students_list.find_by_id(i + 1)
+  puts student
+end
+
+# Сортировка студентов по фамилии и инициалам
+puts "\nСортируем студентов по фамилии и инициалам:"
+students_list.sort_by_initials!
+
+# Вывод отсортированного списка
+puts "\nСписок студентов после сортировки:"
+students_list.count.times do |i|
+  student = students_list.find_by_id(i + 1)
+  puts student
+end
+
+# Получение короткого списка
+puts "\nПолучение короткого списка студентов (первые 2 записи):"
+short_list = students_list.get_k_n_student_short_list(1, 2)
+puts short_list.get_data.to_s
+
+# Сохраняем изменения в файл
+students_list.save_to_file
+puts "\nИзменения сохранены в файл '#{TEST_JSON_FILE}'."
 
 
