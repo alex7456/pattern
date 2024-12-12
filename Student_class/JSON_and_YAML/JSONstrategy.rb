b8bc0ccef12f10c5669d576ed183b598c05d9fba
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-12 21:58:13 +0300

    create JSONstrategy.rb L4T2.6

---
+++ Student_class/JSON_and_YAML/JSONstrategy.rb
@@ -0,0 +1,37 @@
+require 'json'
+require_relative 'file_strategy'
+class JSONStrategy < FileStrategy
+  def load(file_path)
+    return [] unless File.exist?(file_path)
+
+    data = JSON.parse(File.read(file_path), symbolize_names: true)
+    data.map do |student_data|
+      Student.new(
+        first_name: student_data[:first_name],
+        last_name: student_data[:last_name],
+        surname: student_data[:surname],
+        phone: student_data[:phone],
+        telegram: student_data[:telegram],
+        email: student_data[:email],
+        git: student_data[:git],
+        birthdate: student_data[:birthdate],
+        id: student_data[:id]
+      )
+    end
+  end
+
+  def save(file_path, students)
+    serialized_data = students.map do |student|
+      {
+        id: student.id,
+        first_name: student.first_name,
+        last_name: student.last_name,
+        surname: student.surname,
+        contact: student.contact,
+        git: student.git,
+        birthdate: student.birthdate
+      }
+    end
+    File.write(file_path, JSON.pretty_generate(serialized_data))
+  end
+end
