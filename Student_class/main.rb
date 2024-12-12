e362bf32941155b446a3d08c598b37a5936ee260
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-12 21:59:31 +0300

    refact main.rb test strategy

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -9,223 +9,64 @@ require_relative 'DataList/data_list_student_short'
 require_relative 'Human'
 require_relative 'student_list_json'
 require_relative 'student_list_yaml'
+require_relative 'StudentsListBase'
+require_relative 'JSONStrategy'
+require_relative 'YAMLStrategy'
+require_relative 'file_strategy'
+# Создаем тестовые данные студентов
 student1 = Student.new(first_name: "Иван", surname: "Иванов", last_name: "Иванович", birthdate: "2000-05-15")
-student2 = Student.new(first_name: "Петр", surname: "Петров", last_name: "Петрович",birthdate: "1998-08-20")
+student2 = Student.new(first_name: "Петр", surname: "Петров", last_name: "Петрович", birthdate: "1998-08-20")
 student3 = Student.new(first_name: "Алексей", surname: "Алексеев", last_name: "Алексеевич", birthdate: "2002-03-10")
 student4 = Student.new(first_name: "Мария", surname: "Сидорова", last_name: "Александровна", birthdate: "1997-12-31")
+
+# Создаем дерево студентов для сортировки
 tree = StudentTree.new
 tree.add(student1)
 tree.add(student2)
 tree.add(student3)
 tree.add(student4)
+
 puts "Все студенты в порядке даты рождения:"
 tree.each { |student| puts "#{student.surname} #{student.birthdate}" }
-filtr_students=tree.select{|student| student.birthdate > "1997-12-31"}
-filtr_students.each {|student| puts student}
 
-# Создание студентов
-studentshort1=Student_short.from_student(student)
-studentshort3=Student_short.from_string(string)
-list_student_short = Data_list_student_short.new([studentshort1, studentshort3])
-# Тестируем выбор студентов
-list_student_short.select(0)
-puts "Выбранные: #{list_student_short.get_selected}"
+# Фильтруем студентов по дате рождения
+puts "\nСтуденты, рожденные после 1997-12-31:"
+filtr_students = tree.select { |student| student.birthdate > "1997-12-31" }
+filtr_students.each { |student| puts student }
 
-list_student_short.select(1)
-puts "Выбранные: #{list_student_short.get_selected}"
+# Тестирование стратегии JSON
+puts "\nТестирование стратегии JSON:"
+json_strategy = JSONStrategy.new
+json_list = StudentsListBase.new('students.json', json_strategy)
 
+# Добавляем студентов в список JSON
+json_list.add_student(student1)
+json_list.add_student(student2)
+json_list.add_student(student3)
+json_list.add_student(student4)
 
-puts list_student_short.get_names
-# Получаем данные
-puts "Таблица выбранных студентов:"
-table = list_student_short.get_data
-puts table.to_s
+# Загружаем и выводим список из JSON
+puts "Студенты из JSON:"
+json_list.load_from_file
+json_list.students.each { |student| puts student }
 
-# Создаем файл JSON для тестирования
-TEST_JSON_FILE = 'students_test.json'
+# Тестирование стратегии YAML
+puts "\nТестирование стратегии YAML:"
+yaml_strategy = YAMLStrategy.new
+yaml_list = StudentsListBase.new('students.yaml', yaml_strategy)
 
-# Создаем тестовый JSON-файл, если он не существует
-unless File.exist?(TEST_JSON_FILE)
-  test_data = [
-    {
-      id: 1,
-      first_name: "Иван",
-      last_name: "Иванов",
-      surname: "Иванович",
-      phone: "111-222-333",
-      telegram: "@ivanov",
-      email: "ivanov@ya.ru",
-      git: "https://github.com/ivanov",
-      birthdate: "2000-05-15"
-    },
-    {
-      id: 2,
-      first_name: "Петр",
-      last_name: "Петров",
-      surname: "Петрович",
-      phone: "222-333-444",
-      telegram: "@petrov",
-      email: "petrov@ya.ru",
-      git: "https://github.com/petrov",
-      birthdate: "1998-08-20"
-    }
-  ]
-  File.write(TEST_JSON_FILE, JSON.pretty_generate(test_data))
-end
+# Добавляем студентов в список YAML
+yaml_list.add_student(student1)
+yaml_list.add_student(student2)
+yaml_list.add_student(student3)
+yaml_list.add_student(student4)
 
-# Создаем объект для работы с JSON
-students_list = StudentsListJSON.new(TEST_JSON_FILE)
+# Загружаем и выводим список из YAML
+puts "Студенты из YAML:"
+yaml_list.load_from_file
+yaml_list.students.each { |student| puts student }
 
-# Загружаем и выводим список студентов
-puts "Загруженные студенты:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Добавляем нового студента
-puts "\nДобавляем нового студента:"
-new_student = Student.new(
-  first_name: "Мария",
-  last_name: "Сидорова",
-  surname: "Александровна",
-  phone: "333-444-555",
-  telegram: "@sidorova",
-  email: "sidorova@ya.ru",
-  git: "https://github.com/sidorova",
-  birthdate: "1997-12-31"
-)
-students_list.add_student(new_student)
-
-# Вывод списка студентов после добавления
-puts "\nСписок студентов после добавления нового:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Удаляем студента по ID
-puts "\nУдаляем студента с ID 2:"
-students_list.delete_by_id(2)
-
-# Вывод списка студентов после удаления
-puts "\nСписок студентов после удаления:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Сортировка студентов по фамилии и инициалам
-puts "\nСортируем студентов по фамилии и инициалам:"
-students_list.sort_by_initials!
-
-# Вывод отсортированного списка
-puts "\nСписок студентов после сортировки:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Получение короткого списка
-puts "\nПолучение короткого списка студентов (первые 2 записи):"
-short_list = students_list.get_k_n_student_short_list(1, 2)
-puts short_list.get_data.to_s
-
-# Сохраняем изменения в файл
-students_list.save_to_file
-puts "\nИзменения сохранены в файл '#{TEST_JSON_FILE}'."
-
-TEST_YAML_FILE = 'students_test.yaml'
-
-# Создаем тестовый JSON-файл, если он не существует
-unless File.exist?(TEST_YAML_FILE)
-  test_data = [
-    {
-      id: 1,
-      first_name: "Иван",
-      last_name: "Иванов",
-      surname: "Иванович",
-      phone: "111-222-333",
-      telegram: "@ivanov",
-      email: "ivanov@ya.ru",
-      git: "https://github.com/ivanov",
-      birthdate: "2000-05-15"
-    },
-    {
-      id: 2,
-      first_name: "Петр",
-      last_name: "Петров",
-      surname: "Петрович",
-      phone: "222-333-444",
-      telegram: "@petrov",
-      email: "petrov@ya.ru",
-      git: "https://github.com/petrov",
-      birthdate: "1998-08-20"
-    }
-  ]
-  File.write(TEST_YAML_FILE, YAML.dump(test_data))
-end
-
-# Создаем объект для работы с JSON
-students_list = StudentsListYAML.new(TEST_YAML_FILE)
-
-# Загружаем и выводим список студентов
-puts "Загруженные студенты:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Добавляем нового студента
-puts "\nДобавляем нового студента:"
-new_student = Student.new(
-  first_name: "Мария",
-  last_name: "Сидорова",
-  surname: "Александровна",
-  phone: "333-444-555",
-  telegram: "@sidorova",
-  email: "sidorova@ya.ru",
-  git: "https://github.com/sidorova",
-  birthdate: "1997-12-31"
-)
-students_list.add_student(new_student)
-
-# Вывод списка студентов после добавления
-puts "\nСписок студентов после добавления нового:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Удаляем студента по ID
-puts "\nУдаляем студента с ID 2:"
-students_list.delete_by_id(2)
-
-# Вывод списка студентов после удаления
-puts "\nСписок студентов после удаления:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Сортировка студентов по фамилии и инициалам
-puts "\nСортируем студентов по фамилии и инициалам:"
-students_list.sort_by_initials!
-
-# Вывод отсортированного списка
-puts "\nСписок студентов после сортировки:"
-students_list.count.times do |i|
-  student = students_list.find_by_id(i + 1)
-  puts student
-end
-
-# Получение короткого списка
-puts "\nПолучение короткого списка студентов (первые 2 записи):"
-short_list = students_list.get_k_n_student_short_list(1, 2)
-puts short_list.get_data.to_s
-
-# Сохраняем изменения в файл
-students_list.save_to_file
-puts "\nИзменения сохранены в файл '#{TEST_YAML_FILE}'."
-
-
+# Проверка сортировки в YAML
+puts "\nСортировка студентов из YAML по фамилии:"
+yaml_list.sort_by_initials!
+yaml_list.students.each { |student| puts "#{student.surname} #{student.first_name} #{student.last_name}" }
