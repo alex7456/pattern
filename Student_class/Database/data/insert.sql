20391c338f21566c79811c2aa80676d141d8229a
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-15 14:40:14 +0300

    inserting data to table L4T3.2

---
+++ Student_class/Database/data/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO student (last_name, first_name, surname, phone, email, telegram, github, birth_date)
+VALUES 
+('Иванов', 'Иван', 'Иванович', '+79111234567', 'ivan.ivanov@example.com', '@ivanov_ivan', 'https://github.com/ivanovivan', '1995-03-10'),
+('Петров', 'Петр', 'Петрович', '+79111234568', 'petr.petrov@example.com', '@petrov_petr', 'https://github.com/petrovpetr', '1996-04-15'),
+('Сидоров', 'Алексей', 'Алексеевич', '+79111234569', 'alexey.sidorov@example.com', '@sidorov_alex', 'https://github.com/sidorovalex', '1997-05-20'),
+('Кузнецов', 'Дмитрий', 'Дмитриевич', '+79111234570', 'dmitry.kuznetsov@example.com', '@kuznetsov_dmitry', 'https://github.com/kuznetsovdmitry', '1998-06-25'),
+('Смирнов', 'Николай', 'Николаевич', '+79111234571', 'nikolay.smirnov@example.com', '@smirnov_nikolay', 'https://github.com/smirnovnikolay', '1999-07-30'),
+('Федоров', 'Антон', 'Антонович', '+79111234572', 'anton.fedorov@example.com', '@fedorov_anton', 'https://github.com/fedorovanton', '1995-08-05'),
+('Васильев', 'Сергей', 'Сергеевич', '+79111234573', 'sergey.vasiliev@example.com', '@vasiliev_sergey', 'https://github.com/vasilievsergey', '1996-09-10'),
+('Морозов', 'Андрей', 'Андреевич', '+79111234574', 'andrey.morozov@example.com', '@morozov_andrey', 'https://github.com/morozovandrey', '1997-10-15'),
+('Новиков', 'Игорь', 'Игоревич', '+79111234575', 'igor.novikov@example.com', '@novikov_igor', 'https://github.com/novikovigor', '1998-11-20'),
+('Орлов', 'Максим', 'Максимович', '+79111234576', 'maksim.orlov@example.com', '@orlov_maksim', 'https://github.com/orlovmaksim', '1999-12-25');
