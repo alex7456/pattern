95aa2ea69190e106090f22cea093ad87f53692cf
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-15 14:34:20 +0300

    create table student L4T3.2

---
+++ Student_class/Database/structure/db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE student (
+	id SERIAL PRIMARY KEY,
+    last_name TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    phone TEXT UNIQUE,
+    email TEXT UNIQUE,
+    telegram TEXT UNIQUE,
+    github TEXT UNIQUE,
+    birth_date DATE NOT NULL
+);
