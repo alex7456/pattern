241ba8930087b12785e581341adb59b16c275f80
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-19 22:00:32 +0300

    add adapter for strategy L4T4

---
+++ Adapter/student_list_adapter.rb
@@ -0,0 +1,33 @@
+require_relative 'Adapter'
+class Students_list_adapter < Adapter
+  def initialize(file_path, strategy)
+    @students_list = Students_list.new(strategy, file_path) # Исправленный порядок
+  end
+
+  def find_student_by_id(id)
+    @students_list.find_student_by_id(id)
+  end
+
+  def get_k_n_student_short_list(k, n)
+    @students_list.get_k_n_student_short_list(k, n)
+  end
+
+  def add_student(student)
+    @students_list.add_student(student)
+    @students_list.save
+  end
+
+  def update_student_by_id(id, updated_student)
+    @students_list.update_student_by_id(id, updated_student)
+    @students_list.save
+  end
+
+  def delete_student_by_id(id)
+    @students_list.delete_student_by_id(id)
+    @students_list.save
+  end
+
+  def get_student_short_count
+    @students_list.get_student_short_count
+  end
+end
