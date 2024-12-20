43f282064cb26a7d569820fb4bb0a3ca8c69325a
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-20 12:47:24 +0300

    refact test.rb L3T2

--- polymorhism_blocks_collections_tests/methods/test.rb
+++ polymorhism_blocks_collections_tests/methods/test.rb
@@ -3,45 +3,45 @@ require_relative './arrayprocessor'
 
 class TestArrayProcessor < Minitest::Test
   def test_elements
-    processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+    processor = Arrayprocessor.new([1, 2, 3, 4, 5])
     assert_equal [1, 2, 3, 4, 5], processor.elements
   end
 
   def test_chunk
-    processor = ArrayProcessor.new([1, 2, 2, 3, 3, 4])
+    processor = Arrayprocessor.new([1, 2, 2, 3, 3, 4])
     result = processor.chunk { |n| n.even? }
     expected = [[1], [2, 2], [3, 3], [4]]
     assert_equal expected, result
   end
 
   def test_include?
-    processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+    processor = Arrayprocessor.new([1, 2, 3, 4, 5])
     assert processor.include?(3)
     refute processor.include?(6)
   end
 
   def test_reduce
-    processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+    processor = Arrayprocessor.new([1, 2, 3, 4, 5])
     result = processor.reduce(0) { |sum, n| sum + n }
     expected = 15
     assert_equal expected, result
   end
 
   def test_sum
-    processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+    processor = Arrayprocessor.new([1, 2, 3, 4, 5])
     result = processor.sum
     expected = 15
     assert_equal expected, result
   end
 
   def test_member?
-    processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+    processor = Arrayprocessor.new([1, 2, 3, 4, 5])
     assert processor.member?(4)
     refute processor.member?(10)
   end
 
   def test_filter
-    processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+    processor = Arrayprocessor.new([1, 2, 3, 4, 5])
     result = processor.filter { |n| n > 3 }
     expected = [4, 5]
     assert_equal expected, result
