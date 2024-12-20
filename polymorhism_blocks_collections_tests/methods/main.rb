f2afb0bf26f0c169788cec1a216e4406a9b47e6a
alex7456 <aleksandrstrrjnyj@gmail.com>
2024-12-20 22:27:33 +0300

    refact L3T2

--- polymorhism_blocks_collections_tests/methods/main.rb
+++ polymorhism_blocks_collections_tests/methods/main.rb
@@ -1,9 +1,9 @@
-require_relative 'ArrayProcessor'
+require_relative 'arrayprocessor'
 def main
-  processor = ArrayProcessor.new([1, 2, 3, 4, 5])
+  processor = Arrayprocessor.new([1, 3, 5, 2, 4])
 
   puts "Элементы массива: #{processor.elements}"
-  puts "Чанк по четности: #{processor.chunk { |n| n.even? }}"
+  puts "Чанк по четности: #{processor.chunk { |n| n.even? }.to_a}"
   puts "Включает 3? #{processor.include?(3)}"
   puts "Сумма всех элементов: #{processor.sum}"
   puts "Член массива 10? #{processor.member?(10)}"
@@ -11,3 +11,4 @@ def main
   puts "Сумма элементов с reduce: #{processor.reduce(0) { |sum, n| sum + n }}"
 end
 main
+
