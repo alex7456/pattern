b3dbeb65e73ecbf52eb914a61a9b53e72f1dbd26
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-01-16 17:59:47 +0300

    L5T0.5

--- Student_class/main.rb
+++ Student_class/main.rb
@@ -15,6 +15,9 @@ require_relative './Database/Connection'
 require_relative './Database/StudentListDB'
 require_relative './DataStructure/student_list_adapter'
 require_relative './DataStructure/student_list_adapter_DB'
+require_relative './Filter/Filter'
+require_relative './Filter/FilterDecorator'
+require_relative './Filter/empty_github_filter'
 require_relative 'student_app'
 require 'pg'
 
@@ -26,6 +29,32 @@ include Fox
 
 
 begin
+  yaml_strategy = YAMLStrategy.new
+  list_adapter1 = Students_list_adapter.new('students_test.yaml', yaml_strategy)
+
+
+
+  base=Filter.new()
+  filter=EmptyGithubFilter.new(base)
+  shrt_list = list_adapter1.get_k_n_student_short_list(1, 10, filter)
+  puts shrt_list.get_data
+  puts list_adapter1.get_student_short_count(filter)
+
+  db_config = {
+    host: 'localhost', user: 'postgres', password: '12345', dbname: 'postgres'
+  }
+  con = Students_list_DB.instance(db_config)
+
+
+
+
+  list_adapter2 = Students_list_db_adapter.new(db_config)
+
+
+
+  short_list=list_adapter2.get_k_n_student_short_list(2, 10, filter)
+
+  puts short_list.get_data
   if __FILE__ == $0
     FXApp.new do |app|
       StudentApp.new(app)
