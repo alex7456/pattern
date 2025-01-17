6addff2be1d1745d2ccd9f81e11bb3dc74208522
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-01-17 19:28:22 +0300

    refact

--- Student_class/personbase.rb
+++ Student_class/personbase.rb
@@ -1,39 +1,38 @@
 class Human
-  attr_reader :id , :git
+  attr_reader :id, :github  # <-- Используем `github`, а не `git`
   ID_REGEX = /^\d+$/
   GIT_REGEX = /^https?:\/\/github\.com\/[a-zA-Z0-9_-]+$/
-  def initialize(id:nil, git:nil)
+
+  def initialize(id:nil, github:nil)
     self.id = id if id
-    self.git = git if git
+    self.github = github if github
   end
-  private def id=(value)
+
+  def id=(value)
     if self.class.valid_id?(value)
-    @id = value
+      @id = value
     else
       raise ArgumentError, "id is not valid"
+    end
   end
-    end
-  private def git=(value)
-    if self.class.valid_git?(value)
-      @git = value
-      else raise ArgumentError, "git is not valid"
-    end
 
+  private def github=(value)
+    if self.class.valid_git?(value)
+      @github = value  # <-- Используем `@github`, а не `@git`
+    else
+      raise ArgumentError, "github is not valid"
     end
+  end
 
   def self.valid_id?(id)
-     ID_REGEX.match?(id.to_s)
+    ID_REGEX.match?(id.to_s)
   end
-  def self.valid_git?(git)
-    GIT_REGEX.match?(git)
+
+  def self.valid_git?(github)
+    GIT_REGEX.match?(github)
   end
-  protected def git_present?(git)
-    !git.nil? && !git.empty?
+
+  protected def git_present?(github)
+    !github.nil? && !github.empty?
   end
-  protected def validate?
-    raise NotImplementedError
-  end
-  protected def contact?
-    raise NotImplementedError
-  end
-  end
+end
