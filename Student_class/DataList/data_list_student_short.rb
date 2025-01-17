b8a27a482266c5f847bfdc43e00396d75b7e63d5
alex7456 <aleksandrstrrjnyj@gmail.com>
2025-01-17 17:40:36 +0300

    L5T1.3 add observer pattern

--- Student_class/DataList/data_list_student_short.rb
+++ Student_class/DataList/data_list_student_short.rb
@@ -1,6 +1,33 @@
- require_relative 'data_list'
+require_relative 'data_list'
 require_relative 'data_table'
+
 class Data_list_student_short < DataList
+  attr_accessor :count
+  def initialize(elements)
+    super(elements)
+    @observers = []
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def remove_observer(observer)
+    @observers.delete(observer)
+  end
+
+  def notify
+    @observers.each do |observer|
+      observer.set_table_params(self.column_names, @count)  # Используем self.column_names
+      observer.set_table_data(get_data)
+    end
+  end
+
+  def column_names
+    ["ID", "ФИО", "Контакт", "Git"]  # Заголовки колонок
+  end
+
+  private
 
   def extract_names
     %w[initials contact git]
@@ -14,4 +41,4 @@ class Data_list_student_short < DataList
       student.git
     ]
   end
-  end
+end
